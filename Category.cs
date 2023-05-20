42d8993c3620e554da5222911500beaea9a9821a
sebastian <abaes1998@gmail.com>
2023-05-20 20:59:51 +0200

    catID

---
+++ Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+
+        public Category(int categoryId, string name)
+        {
+            CategoryID = categoryId;
+            Name = name;
+        }
+    }
+}
